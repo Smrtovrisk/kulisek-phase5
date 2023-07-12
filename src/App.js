58565a9f257481c62cfb169eacc48f6295410807
Martin Kulisek <martin.kulisek@vodafone.com>
2023-07-12 15:53:19 +0200

    repair initial commit and handle main branch mismatch

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World! This is FSD Phase 5 final project.</h1>
+     <h2>Author: martin.kulisek@vodafone.com</h2>
+     <h2>Github: <a href="https://github.com/Smrtovrisk/kulisek-phase5">https://github.com/Smrtovrisk/kulisek-phase5</a></h2>
+     <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" alt="Funny gif" />
+    </div>
+  );
+}
+
+export default App;
